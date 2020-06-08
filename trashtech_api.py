359371532b572db18bed5f4b3355a46561d6c8ae
d0minikk <curylo.dominik@gmail.com>
2020-06-09 00:48:02 +0200

    Create trashtech_api.py

---
+++ trashtech_api.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+import requests
+import logging
+import json
+
+logging.basicConfig(format='%(asctime)s - %(message)s', level=logging.INFO)
+
+class TrashtechApi:
+  api_base = 'http://trashtech.herokuapp.com/api'
+
+  def configuration(self):
+    url = "%s/device_configurations/1" % self.api_base
+    logging.info('[INFO] request call: %s' % url)
+
+    response = requests.get(url)
+    json = response.json()
+    logging.info('[INFO] json response: %s' % json)
+
+    return json
+
+  def create_status(device_reference):
+    request_json = {
+      "device_status": {
+        "image_name": "",
+        "image_created_at": "",
+        "image_e_tag": "",
+        "image_size": "",
+        "device_reference": "",
+        "container_identifier_number": "000001",
+      }
+    }
+    logging.info("[INFO] request json: %s", request_json)
+    url = "%s/device_statuses/%s" % (self.api_base, device_reference)
+    logging.info("[INFO] request call: %s", url)
+
+    response = requests.post(url, json = request_json)
+    json = response.json()
+    logging.info('[INFO] json response: %s' % json)
+
+    return json
