26581f4825a54d59d4fc1a58f9382e674095f672
d0minikk <curylo.dominik@gmail.com>
2020-06-09 19:31:52 +0200

    Update main.py

--- main.py
+++ main.py
@@ -12,8 +12,12 @@ from gsm_controller import GsmController
 
 logging.basicConfig(format='%(asctime)s - %(message)s', level=logging.INFO)
 
+TIMESTAMP_FORMAT = '%Y-%m-%d %H:%M:%S'
+DEVICE_REFERENCE = '000006'
+GSM_WARMUP_TIME = 50 # 50 seconds
+
 class TrashtechApp:
-  def __init__(self):
+  def initialize(self):
     self.trashtech_client = TrashtechApi()
     self.s3_client = S3Client()
     self.snapper = Snapper()
@@ -30,12 +34,12 @@ class TrashtechApp:
     self.snapper.snap(filename)
 
   def init_gsm(self):
-    self.gsm_controller.enable()
-
-  def run(self):
     if self.gsm_controller.is_ppp_interface_present() is not True:
       self.init_gsm()
-      time.sleep(50)
+      time.sleep(GSM_WARMUP_TIME)
+
+  def run(self):
+    self.init_gsm()
 
     configuration = self.trashtech_client.configuration()
 
@@ -43,19 +47,19 @@ class TrashtechApp:
     complete_file_path = 'TT_%s.jpg' % image_created_at_timestamp
 
     self.call_snap(complete_file_path)
-    time.sleep(5)
+    time.sleep(1)
 
     response = self.s3_client.upload(complete_file_path)
 
-    device_reference = '000006'
-    image_created_at = datetime.datetime.fromtimestamp(image_created_at_timestamp).strftime('%Y-%m-%d %H:%M:%S')
-    self.trashtech_client.create_status(device_reference, response.e_tag, complete_file_path, image_created_at)
+    image_created_at = datetime.datetime.fromtimestamp(image_created_at_timestamp).strftime(TIMESTAMP_FORMAT)
+    self.trashtech_client.create_status(DEVICE_REFERENCE, response.e_tag, complete_file_path, image_created_at)
 
     self.gsm_controller.disable()
 
-    thread = threading.Timer(configuration['photo_interval']-50, trashtech_app.run)
+    thread = threading.Timer(configuration['photo_interval'] - GSM_WARMUP_TIME, trashtech_app.run)
     thread.start()
 
 if __name__ == '__main__':
   trashtech_app = TrashtechApp()
+  trashtech_app.initialize()
   trashtech_app.run()
